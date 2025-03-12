c9c08c0e982e844d46bae94e03ef549db5e704a9
breeze-wind <3792287393@qq.com>
2025-03-13 01:18:07 +0800

    可视化阶段

--- src/answer/src/show.cpp
+++ src/answer/src/show.cpp
@@ -7,11 +7,11 @@
 #include"opencv2/opencv.hpp"
 #include<memory>
 
-class picture_show : public rclcpp::Node {
+class image_precss : public rclcpp::Node {
 public:
-    picture_show() : rclcpp::Node("image_subscriber") {
+    image_precss() : rclcpp::Node("image_subscriber") {
         subscription_ = create_subscription<sensor_msgs::msg::Image>("raw_image", 10,
-                                                                     std::bind(&picture_show::image_callback, this,
+                                                                     std::bind(&image_precss::image_callback, this,
                                                                                std::placeholders::_1));
     }
         private:
@@ -21,11 +21,22 @@ public:
           try {
               cv::Mat frame=cv_bridge::toCvCopy(msg,"bgr8")->image;
               cv::imshow("image",frame);
+              cv::Mat imag1, imag2,imag3;
+              cv::Scalar low, high;
+              low = cv::Scalar(250,190,10);
+              high = cv::Scalar(256,200,20);
+              cv::inRange(frame,low,high,imag1);
+              cv::inRange(frame,high,low,imag2);
+              cv::bitwise_or(imag1,imag2,imag1);//合并，现在imag1是处理过的蓝色框
+
+              low=cv::Scalar(250,250,250);
+              high=cv::Scalar(255,255,255);
+              cv::inRange(frame,low,high,imag3);
+              cv::bitwise_or(imag3,imag1,imag1);
+              cv::imshow("image1",imag1);
               cv::waitKey(1);
-              static int count = 0;
-              if (++count%30 == 0) {
-                  cv::imwrite("/home/sfx233/test"+std::to_string(count)+".jpg",frame);
-              }
+
+
           }catch (const cv_bridge::Exception& e){
                   RCLCPP_INFO(this->get_logger(),"cv转化失败%s",e.what());
               }
@@ -35,7 +46,7 @@ public:
 
 int main(int argc, char **argv) {
     rclcpp::init(argc,argv);
-    rclcpp::spin(std::make_shared<picture_show>());
+    rclcpp::spin(std::make_shared<image_precss>());
     rclcpp::shutdown();
     cv::destroyAllWindows();
 }
